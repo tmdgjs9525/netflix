fd4807075e02b14264e41d7335825ff5e71219aa
tmdgjs9525 <56830330+tmdgjs9525@users.noreply.github.com>
2025-05-12 23:12:34 +0900

    opensilver ai 테스트

--- netflix.Main/ViewModels/BookMarkedViewModel.cs
+++ netflix.Main/ViewModels/BookMarkedViewModel.cs
@@ -1,12 +1,32 @@
-﻿using netflix.Core;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using netflix.Core;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace netflix.Main.ViewModels
 {
+    public partial class Test : ObservableObject
+    {
+        [ObservableProperty]
+        public partial string Title { get; set; }
+
+        [ObservableProperty]
+        public partial string Description { get; set; }
+    }
+
     public partial class BookMarkedViewModel : ViewModelBase
     {
+        
+        [ObservableProperty]
+        public partial ObservableCollection<Test> MyList { get; set; } = new ObservableCollection<Test>();
+
+
         public BookMarkedViewModel()
         {
-
+            MyList.Add(new Test() { Title = "Title1", Description = "Description1"});
+            MyList.Add(new Test() { Title = "Title2", Description = "Description2" });
+            MyList.Add(new Test() { Title = "Title3", Description = "Description3" });
+            MyList.Add(new Test() { Title = "Title4", Description = "Description4" });
         }
     }
 }
--- netflix.Main/Views/BookmarkedView.xaml.cs
+++ netflix.Main/Views/BookmarkedView.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows.Controls;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
 
 namespace netflix.Main.Views
 {
     public partial class BookMarkedView : UserControl
     {
+
         public BookMarkedView()
         {
             this.InitializeComponent();
